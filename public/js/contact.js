bc0a48f52603289d8628fec3b227435a55336f8e
MainaPaina <Maina.Kodehode@gmail.com>
2025-03-25 02:28:46 +0100

    added contact.css and contact.js file

--- public/js/contact.js
+++ public/js/contact.js
@@ -7,7 +7,7 @@ function toggleFaqQuestions(faqAnswerID)
     // store the DOM element in a variable
     let faqAnswer = document.getElementById(faqAnswerID);
     // toggle the class faq-answer-visible
-    faqAnswer.classList.toggle("faq-answer-visible");
+    //faqAnswer.classList.toggle("faq-answer-visible");
 
     // another way to do the same thing
     /*if (faqAnswer.style.display === "block")
